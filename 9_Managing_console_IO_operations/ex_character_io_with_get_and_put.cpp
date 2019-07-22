2ab9191124b74429ab30399631f209166f2f3d90
Tejas Madrewar <tejas.madrewar@gmail.com>
2019-07-22 20:58:48 +0530

    get() and put()

---
+++ 9_Managing_console_IO_operations/ex_character_io_with_get_and_put.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int count = 0;
+	char c;
+
+	cout << "INPUT TEXT\n";
+
+	cin.get(c);
+
+	while(c!='\n'){
+		cout.put(c);
+		count++;
+		cin.get(c);
+	}
+
+	cout << "\nNumber of characters = " << count;
+
+	return 0;
+}
